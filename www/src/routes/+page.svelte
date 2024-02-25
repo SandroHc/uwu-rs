b86c87c62a42fb4d3e6d7838e8a20bd64f172d80
Sandro Marques <sandro123iv@gmail.com>
2024-02-25 12:31:24 +0000

    chore(web): Remove dead code

--- www/src/routes/+page.svelte
+++ www/src/routes/+page.svelte
@@ -1,17 +1,6 @@
 <script lang="ts">
-	// import svelteLogo from "../assets/svelte.svg";
-	// import viteLogo from "../assets/vite.svg";
 	import Translator from "$lib/components/Translator.svelte";
 </script>
-
-<!--<div>-->
-<!--	<a href="https://vitejs.dev" target="_blank" rel="noreferrer">-->
-<!--		<img src={viteLogo} class="logo" alt="Vite Logo"/>-->
-<!--	</a>-->
-<!--	<a href="https://svelte.dev" target="_blank" rel="noreferrer">-->
-<!--		<img src={svelteLogo} class="logo svelte" alt="Svelte Logo"/>-->
-<!--	</a>-->
-<!--</div>-->
 
 <div class="header">
 	uwuify
